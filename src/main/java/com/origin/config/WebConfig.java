9a05b747c5b2f21979d14d8f62b0c197043d7863
Djiba <sylvestre.ponte@gmail.com>
2025-05-05 18:11:06 +0200

    mis à jour des cors

--- src/main/java/com/origin/config/WebConfig.java
+++ src/main/java/com/origin/config/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "http://localhost:5173")
-                        .allowedMethods("*")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
